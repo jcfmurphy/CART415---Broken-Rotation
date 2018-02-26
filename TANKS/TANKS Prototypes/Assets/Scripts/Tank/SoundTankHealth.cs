72808013620dfd3353ad9977f07ef0a5f6ab249d
jcfmurphy <jcfmurphy@gmail.com>
2018-02-26 17:30:37 -0500

    UI updates

--- TANKS/TANKS Prototypes/Assets/Scripts/Tank/SoundTankHealth.cs
+++ TANKS/TANKS Prototypes/Assets/Scripts/Tank/SoundTankHealth.cs
@@ -4,6 +4,14 @@ using UnityEngine.UI;
 public class SoundTankHealth : TankHealth
 {
 	private bool m_InContact;
+	public Color[] m_FlashColors = new Color[2];
+	private Color m_StartColor;
+	MeshRenderer[] m_Renderers;
+
+	void Start() {
+		m_Renderers = this.gameObject.GetComponentsInChildren<MeshRenderer> ();
+		m_StartColor = m_Renderers [0].material.color;
+	}
 
 	void OnCollisionEnter(Collision collisionInfo) {
 		if (collisionInfo.gameObject.name == "Sound King Tank(Clone)") {
@@ -14,13 +22,23 @@ public class SoundTankHealth : TankHealth
 	void OnCollisionExit(Collision collisionInfo) {
 		if (collisionInfo.gameObject.name == "Sound King Tank(Clone)") {
 			m_InContact = false;
+			for (int i = 0; i < m_Renderers.Length; i++)
+			{
+				m_Renderers[i].material.color = m_StartColor;
+			}
 		}
 	}
 
 	public void Update() {
 		if (m_InContact) {
-			float damage = Time.deltaTime * 50;
+			float damage = Time.deltaTime * 100;
 			TakeDamage (damage);
+
+			Color tempColor = m_FlashColors[Random.Range(0, m_FlashColors.Length)];
+			for (int i = 0; i < m_Renderers.Length; i++)
+			{
+				m_Renderers[i].material.color = tempColor;
+			}
 		}
 	}
 }
