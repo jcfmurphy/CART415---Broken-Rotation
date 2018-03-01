825de5e3942cedbb12d8d8d2015abffc8bd946c9
jcfmurphy <jcfmurphy@gmail.com>
2018-02-28 21:58:33 -0500

    Started 3D object prototype scene

---
+++ TANKS/TANKS Prototypes/Assets/Scripts/RotateCube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour {
+
+	private float m_YRotateValue;
+	private float m_XRotateValue;
+	private float m_RotateSpeed = 180f;
+	private Transform m_Transform;
+
+
+	// Use this for initialization
+	void Start () {
+		m_Transform = this.gameObject.GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_XRotateValue = Input.GetAxis ("Vertical2");
+		m_YRotateValue = Input.GetAxis ("Horizontal2");
+
+		float xTurn = m_XRotateValue * m_RotateSpeed * Time.deltaTime;
+		float yTurn = m_YRotateValue * m_RotateSpeed * Time.deltaTime;
+
+		Vector3 turnRotation = new Vector3 (xTurn, yTurn, 0f);
+
+		m_Transform.Rotate (turnRotation, Space.World);
+	}
+}
